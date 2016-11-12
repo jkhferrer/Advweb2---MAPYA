6cc6d01ed654ffd083e7cc9a99bb33eb5d98d17c
mmlaureta <lletty02@gmail.com>
2016-11-13 03:05:37 +0800

    bus and van

---
+++ Adweb2/PROJECT-TRAVEL LITE/Application/Travel_Lite/application/views/travel/transaction.php
@@ -0,0 +1,15 @@
+
+
+<p> From: </p> <br/>
+<form action="###">
+  <select name="from"> placeholder="Departing From">
+    <option value="manila" selected>Manila</option>
+    <option value="lucena">Lucena</option>
+    <option value="ilocos">Ilocos</option>
+    <option value="pampanga">Pampanga</option>
+  </select>
+  <br><br>
+  <input type="submit">
+</form>
+
+
