c17befba3f1569a59ed56f88afc6765dbf3fbd14
spacejam101 <allenbaldovino101@gmail.com>
2016-11-15 23:34:27 +0800

    Letty chan update

--- PROJECT-TRAVEL LITE/Application/Travel_Lite/application/views/travel/rentals.php
+++ PROJECT-TRAVEL LITE/Application/Travel_Lite/application/views/travel/rentals.php
@@ -1,3 +1,7 @@
+
+<?php
+$this->load->helper('url');
+?>
 <!-- header -->
 <div class="banner-1">
 <div class="container">
@@ -8,11 +12,16 @@
 				<div class="head-nav">
 						<span class="menu"> </span>
 							<ul class="cl-effect-16">
-								<li><a href="<?php echo base_url('index.php');?>" data-hover="HOME">HOME</a></li>
-								<li><a href="<?php $this->load->helper('url'); echo base_url('index.php/about/view');?>" data-hover="ABOUT">ABOUT</a></li>
-								<li class="active"><a href="<?php $this->load->helper('url'); echo base_url('index.php/rentals/view');?>" data-hover="RENTALS">RENTALS</a></li>
-								<li><a href="<?php $this->load->helper('url'); echo base_url('index.php/destination/view');?>" data-hover="DESTINATIONS">DESTINATIONS</a></li>
-								<li><a href="<?php $this->load->helper('url'); echo base_url('index.php/contact/view');?>" data-hover="CONTACTS">CONTACTS</a></li>
+								<li>
+									<a href="<?php echo base_url('index.php');?>" data-hover="HOME">HOME
+									</a>
+								</li>
+								<li>
+									<a href="<?php  echo base_url('index.php/about/view');?>" data-hover="ABOUT">ABOUT</a>
+								</li>
+								<li class="active"><a href="<?php echo base_url('index.php/rentals/view');?>" data-hover="RENTALS">RENTALS</a></li>
+								<li><a href="<?php echo base_url('index.php/destination/view');?>" data-hover="DESTINATIONS">DESTINATIONS</a></li>
+								<li><a href="<?php  echo base_url('index.php/contacts/view');?>" data-hover="CONTACTS">CONTACTS</a></li>
 									<div class="clearfix"> </div>
 							</ul>
 				</div>
@@ -30,15 +39,26 @@
 			</div>
   </div>
 <!-- header -->
-<div class="404">
+
+<!-- transaction -->
+<div class="transaction">
 <div class="container">
-			<!--start-plans-404page---->
-			<div class="error-page">
-				<p>Under Construction! :(</p>
-				<h3>404</h3>
-				<a href="<?php echo base_url('index.php');?>" class="btn  btn-1c">Go home</a>
-			</div>
-			<!--End-plans-404page---->
+	<div class="hover11 column">
+	<center><label>Choose your Vehicle: </label></center>
+		<div class="bus">
+			<figure><img src="<?php echo base_url(); ?>assests/images/bus.png" style="height:200px; width:400px;"/></figure>
+
+				<form action="<?php echo base_url('index.php/transaction/view');?>">
+				<button class="private">Private </button>
+				</form>
+
+				<button class="public"> Public </button>
+			
+		</div>
+		<div>
+			<figure><img src="<?php echo base_url(); ?>assests/images/van.png" style="height:200px; width: 350px;"/></figure>
+		</div><br/>
+		  </div>
 			<div class="clearfix"> </div>
 		</div>
 	</div>
